40b0ab825ecf24a828fa20b21873b112500d6fe3
Aryan Vinod <ayevinny@Aryans-MacBook-Air-4.local>
2025-03-12 17:27:52 -0700

    modified models.py

---
+++ models.py
@@ -0,0 +1,27 @@
+from app import db
+from datetime import datetime, timezone
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    password = db.Column(db.String(200), nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    events = db.relationship('Event', backref='organizer', lazy=True)
+
+class Event(db.Model):
+    __tablename__ = 'events'
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(150), nullable=False)
+    description = db.Column(db.Text, nullable=True)
+    time = db.Column(db.DateTime, default=datetime.utcnow)
+    location = db.Column(db.String(200), nullable=False)
+    organizer_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    rsvps = db.relationship('RSVP', backref='event', lazy=True)
+
+class RSVP(db.Model):
+    __tablename__ = 'rsvps'
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    event_id = db.Column(db.Integer, db.ForeignKey('events.id'), nullable=False)
+    timestamp = db.Column(db.DateTime, default=lambda: datetime.now(timezone.utc))
